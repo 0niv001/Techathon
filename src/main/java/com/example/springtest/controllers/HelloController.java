b5064de4c8668e60f7d1888fd7c72d3734663b58
0niv001 <nirvesh_99@proton.me>
2025-01-14 15:50:28 +0000

    Homepage

--- src/main/java/com/example/springtest/controllers/HelloController.java
+++ src/main/java/com/example/springtest/controllers/HelloController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello" + name;
+    @GetMapping("/index")
+    public String sayHello(Model model) {
+        return "index";
     }
 }
