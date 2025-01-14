b41b58cb40d26638349fa481d8289951dfa15df9
Arafat Hossain Shake Mohammed <arafat.hossain97@gmail.com>
2025-01-14 19:53:02 +0000

    new article pages added and linked

--- src/main/java/com/example/springtest/controllers/MainPageController.java
+++ src/main/java/com/example/springtest/controllers/MainPageController.java
@@ -3,6 +3,7 @@ package com.example.springtest.controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainPageController {
@@ -11,4 +12,26 @@ public class MainPageController {
         return "mainpage";
     }
 
+    @GetMapping("/article/investmentArticle")
+    public String investmentArticle(Model model) {
+        return "investmentArticle";
+
+    }
+
+    @GetMapping("/article/mortgagesArticle")
+    public String mortgagesArticle(Model model) {
+        return "mortgagesArticle";
+
+    }
+    @GetMapping("/article/pensionArticle")
+    public String pensionArticle(Model model) {
+        return "pensionArticle";
+
+    }
+
+    @GetMapping("/article/savingsArticle")
+    public String savingsArticle(Model model) {
+        return "savingsArticle";
+
+    }
 }
