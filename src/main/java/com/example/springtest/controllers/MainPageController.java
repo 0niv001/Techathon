20216d6e1ce408a5d07b25539217ad83c6e70c9d
Arafat Hossain Shake Mohammed <arafat.hossain97@gmail.com>
2025-01-16 17:54:22 +0000

    Small Adjustments

--- src/main/java/com/example/springtest/controllers/MainPageController.java
+++ src/main/java/com/example/springtest/controllers/MainPageController.java
@@ -3,42 +3,51 @@ package com.example.springtest.controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
-
+//controller to direct users to the blog page
 @Controller
 public class MainPageController {
     @GetMapping("/mainpage")
     public String mainpage(Model model ) {
         return "blog";
     }
+    //
 
+    //controller to directs users to the article page for investment
     @GetMapping("/article/investmentArticle")
     public String investmentArticle(Model model) {
         return "investmentArticle";
 
     }
 
+    //directs users to mortgages article
     @GetMapping("/article/mortgagesArticle")
     public String mortgagesArticle(Model model) {
         return "mortgagesArticle";
 
     }
+
+    //directs users to the pension article
     @GetMapping("/article/pensionArticle")
     public String pensionArticle(Model model) {
         return "pensionArticle";
 
     }
 
+    //directs users to the savings article
     @GetMapping("/article/savingsArticle")
     public String savingsArticle(Model model) {
         return "savingsArticle";
 
     }
 
+    //directs users to the mortgage calculator
     @GetMapping("/article/mortgagesCalculator")
     public String mortgagesCalculator(Model model) {
         return "mortgagesCalculator";
 
     }
+
+    //directs users to the Course page
     @GetMapping("/course")
     public String course(Model model) {
         return "course";
